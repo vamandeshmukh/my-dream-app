5e596717d45f53ac89b418cb79571d64ed981337
vamandeshmukh <vamandeshmukh@gmail.com>
2021-02-24 11:09:14 +0530

    Commit
    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent {
   age: number = 10;
   isIndian: boolean = false;
   status: string = 'No status';
-  constructor() {
+  isLoggedIn: boolean = false; // delete this 
+  constructor(  ) {
     setTimeout(() => {
       this.isIndian = true;
       console.log(this.isIndian);
